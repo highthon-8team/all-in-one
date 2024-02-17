e6d11b482c77eca546fbb93e50941f3107ead08c
Mijin Sim <80371353+azure-553@users.noreply.github.com>
2024-02-18 07:32:00 +0900

    FEAT : 음성 인식 및 텍스트 출력

--- app/(client)/recordspeaking/page.tsx
+++ app/(client)/recordspeaking/page.tsx
@@ -1,9 +1,22 @@
 "use client";
 
+import { useState } from "react";
 import Image from "next/image";
 import recordVoice from "../_image/recordVoice.svg";
+import { useSpeechRecognition } from "react-speech-kit";
 
 export default function RecordSpeakingPage() {
+  const [voice, setVoice] = useState("");
+  const [isSaved, setIsSaved] = useState(false);
+
+  const { listen, listening, stop } = useSpeechRecognition({
+    onResult: (result: string) => {
+      // 음성인식 결과가 value 상태값으로 할당됩니다.
+      setVoice(result);
+      setIsSaved(true);
+    },
+  });
+
   return (
     <div className="flex flex-col gap-4 self-start text-3xl font-bold w-full whitespace-nowrap mt-32">
       <h1>
@@ -12,19 +25,31 @@ export default function RecordSpeakingPage() {
         말해보세요.
       </h1>
 
-      <div className="mx-auto w-[230px] h-[230px] rounded-full bg-[#7F30FF]">
-        <div className="mx-auto my-[18px] pt-[16px] w-[190px] h-[190px] rounded-full bg-[#1E1E1E]">
-          <div className="mx-auto pt-[36px] w-[160px] h-[160px] rounded-full bg-[#47355D]">
-            <Image
-              className="mx-auto"
-              src={recordVoice}
-              alt="recordVoice.png"
-              width={70}
-              height={70}
-            />
+      {isSaved ? (
+        <div className="w-[100%]" onMouseUp={stop}>
+          {voice}
+        </div>
+      ) : (
+        <div className="mx-auto w-[230px] h-[230px] rounded-full bg-[#7F30FF]">
+          <div className="mx-auto my-[10px] pt-[16px] w-[210px] h-[210px] rounded-full bg-[#1E1E1E]">
+            <div
+              className="mx-auto pt-[46px] w-[180px] h-[180px] rounded-full bg-[#47355D]"
+              onMouseDown={() => {
+                listen({ interimResults: false });
+              }}
+            >
+              <Image
+                className="mx-auto"
+                src={recordVoice}
+                alt="recordVoice.png"
+                width={70}
+                height={70}
+              />
+            </div>
           </div>
         </div>
-      </div>
+      )}
+      {listening && <span>음성인식 활성화 중 ...</span>}
     </div>
   );
 }
